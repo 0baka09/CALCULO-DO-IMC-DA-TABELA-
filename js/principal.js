46ead1056c697a9af4f9b83b9ce94b2a93f71399
Miguel de Oliveira Bigueti <163124025+0baka09@users.noreply.github.com>
2024-09-23 11:36:34 -0300

    all

--- js/principal.js
+++ js/principal.js
@@ -1,8 +1,11 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista"
 
-var Paulo = document.querySelector("#paciente1");
-var Peso1 = Paulo.querySelector(".info-peso").textContent;
-var Altura1 = Paulo.querySelector(".info-altura").textContent;
-var Imc = Peso1 / (Altura1 * Altura1);
-Paulo.querySelector(".info-imc").textContent = Imc;
+var pacientes = document.querySelectorAll(`.paciente`);
+for(let contador =0; contador < pacientes.length; contador++){
+    var peso = pacientes[contador].querySelector(`.info-peso`).textContent;
+    var altura = pacientes[contador].querySelector(`.info-altura`).textContent;
+    var imc = peso / (altura * altura);
+    imc = parseFloat(imc);
+    pacientes[contador].querySelector(`.info-imc`).textContent = imc.toFixed(2);
+}
